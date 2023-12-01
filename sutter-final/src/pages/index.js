4c5e15e27f5895e08e5c5ec8989e9dd3656777a0
Samantha Sutter <sutters4@gmatc.matc.edu>
2023-11-30 21:34:43 -0600

    final

---
+++ sutter-final/src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Hero from "../components/Hero"
+
+
+const IndexPage = () => {
+  <Layout>
+    <Seo title="Home" />
+    <Hero />
+  </Layout>
+}
+
+export default IndexPage
